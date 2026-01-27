038533e08b7f943975f511ee5944fe48f9e3d279
gojosatoru0184847-collab <gojosatoru0184847@gmail.com>
2026-01-28 06:29:07 +0700

    Add files via upload

---
+++ FrameBuffer.js
@@ -0,0 +1,25 @@
+export class FrameBuffer {
+  constructor(gl, w, h){
+    this.gl=gl;
+    this.fbo=gl.createFramebuffer();
+    this.tex=gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, this.tex);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA8, w, h, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, this.fbo);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.tex, 0);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    this.w=w; this.h=h;
+  }
+  resize(w,h){
+    if(this.w===w && this.h===h) return;
+    const gl=this.gl;
+    this.w=w; this.h=h;
+    gl.bindTexture(gl.TEXTURE_2D, this.tex);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA8, w, h, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+  }
+}
