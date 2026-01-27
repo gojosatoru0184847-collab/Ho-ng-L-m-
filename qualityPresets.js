4a25f376da97c7bd7d01a4be8b200edd6c7c4e69
gojosatoru0184847-collab <gojosatoru0184847@gmail.com>
2026-01-28 06:34:00 +0700

    Add files via upload

---
+++ qualityPresets.js
@@ -0,0 +1,5 @@
+export const Quality = {
+  MOBILE_LOW: { dpr: 1, lights: 3, particles: 6000, instances: 6000 },
+  MOBILE_MED: { dpr: 1.5, lights: 5, particles: 12000, instances: 12000 },
+  MOBILE_HIGH:{ dpr: 2, lights: 8, particles: 20000, instances: 20000 },
+};
