7d384bc7296406302076d93ccef5866f980e223b
gojosatoru0184847-collab <gojosatoru0184847@gmail.com>
2026-01-28 21:14:18 +0700

    Delete BufferPool.js

--- BufferPool.js
+++
@@ -1,5 +0,0 @@
-export class BufferPool {
-  constructor(gl){ this.gl=gl; this.free=[]; }
-  alloc(){ return this.free.pop() || this.gl.createBuffer(); }
-  release(buf){ if(buf) this.free.push(buf); }
-}
